8bcec71bf12c1240477144e72479cf6943ca1bc1
btcantrell0 <159195743+btcantrell0@users.noreply.github.com>
2024-02-26 22:06:10 -0600

    Add files via upload

---
+++ testRemove.js
@@ -0,0 +1,44 @@
+async function postRequest(data){
+    let options = {
+        method: 'POST',
+        headers: {
+            'Content-Type':
+                'application/json;charset=utf-8'
+        },
+        body: JSON.stringify(data)
+    }
+
+    try {
+        const response = await fetch('http://localhost:3000', options);
+        const responseData = await response.json();
+        return responseData;
+    } catch (error) {
+        console.error('Error:', error);
+        return { success: false, message: 'An error occured during the request.' };
+    }
+}
+
+async function removeDocument() {
+    const partId = document.getElementById("partIdInput").value.trim();
+    
+    const data = {
+        type: 'remove',
+        input: partId
+    };
+
+    try {
+        const removeResponse = await postRequest(data);
+        console.log("Remove Response: ", removeResponse);
+
+        if (removeResponse.success){
+            document.getElementById('removeResponse').innerText = removeResponse.message;
+        } else {
+            console.error('Removal failed:', removeResponse.message);
+            document.getElementById('removeResponse').innerText = 'Removal failed: ' + removeResponse.message;
+        }
+    } catch (error) {
+        console.error('Error removing document:', error);
+        document.getElementById('removeResponse').innerText = 'An error occured while removing the document.';
+    }
+
+}
