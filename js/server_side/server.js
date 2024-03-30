330aa8d474f278d2aac056b71f39bfc3f16359d5
adamw1221 <141478307+adamw1221@users.noreply.github.com>
2024-03-30 13:23:58 -0500

    Update server.js to not use local host

--- js/server_side/server.js
+++ js/server_side/server.js
@@ -8,7 +8,7 @@ const remove = require("../operations/doc_remove.js");
 
 const bodyParser = require('body-parser');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.static('LabInventory'));
 app.use(cors());
@@ -188,7 +188,7 @@ app.post('/', async(req, res) => {
 });
 
 app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
+    console.log(`Server running on port ${port}`);
 });
 
 /*
