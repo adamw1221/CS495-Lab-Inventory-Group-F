daab75cee8c31e02490a71451fe7fe1b4c2349e1
adamw1221 <141478307+adamw1221@users.noreply.github.com>
2024-04-01 21:09:24 -0500

    Update server.js

--- js/server_side/server.js
+++ js/server_side/server.js
@@ -14,7 +14,7 @@ const port = process.env.PORT || 3000;
 const path = require('path');
 
 app.get("/", function (req, res) {
-res.sendFile(path.join(__dirname,"..","..", "html",'testUpdate.html'));
+res.sendFile(path.join(__dirname,"..","..", "html",'checkoutParts.html'));
 });
 
 //app.use(express.static('LabInventory'));
