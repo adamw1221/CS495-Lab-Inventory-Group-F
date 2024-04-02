38516dc0e73d7d0c1c6b042088af849c88b0b57d
adamw1221 <141478307+adamw1221@users.noreply.github.com>
2024-04-01 21:28:30 -0500

    Update server.js

--- js/server_side/server.js
+++ js/server_side/server.js
@@ -18,7 +18,6 @@ res.sendFile(path.join(__dirname,"..","..", "html",'checkoutParts.html'));
 });
 
 app.use(express.static(path.join(__dirname, '..', '..', 'html')));
-app.use(express.static(path.join(__dirname, '..', '..', 'css')));
 app.use(cors());
 app.use(express.json());
 
